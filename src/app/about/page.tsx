22a1fa0445b25bf8c9e862a8ecfdb89ec1c7b2d7
redouane <radouane.hitachi@gmail.com>
2025-08-01 10:06:05 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,19 @@
-import Link from "next/link"
-
-const AboutPage=()=>{
-    return(
+import Image from "next/image";
+import CloudImage from "../../../public/cloud-hosting.png";
+const AboutPage = () => {
+    return (
         <div>
-            <h1>about page</h1>
+            <section className="fix-height container m-auto">
+                <h1 className="text-3xl font-bold text-gray-800 p-5">About Page</h1>
+                <div>
+                  <Image 
+                  priority
+                  src={CloudImage} 
+                  alt="Cloud Hosting" 
+                  width={500} 
+                  height={500}/>
+                </div>
+            </section>
         </div>
     )
 }
--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,9 +1,28 @@
-const singleArticlePage=(props:string)=>{
-     
+import { Article } from "@/utils/types";
 
+interface singleArticlePageProps{
+    params:{id:string}
+}
+
+const singleArticlePage=async({params}:singleArticlePageProps)=>{
+     const response=await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+    const article:Article =await response.json();
+    if (
+        !response.ok
+    ) {
+        throw new Error("Failed to fetch article")
+        
+    }
     return (
-        <div>Single Article Page</div>
+       <section className="fix-height container m-auto w-full px-5 pt-8 md:w-3/4">
+  <div className="bg-white p-7 rounded-lg">
+    <h1 className="text-3xl font-bold text-gray-700 mb-2">{article.title}</h1>
+    <div className="text-sm text-gray-500">01/01/2024</div>
+    <p className="text-gray-800 text-xl mt-5">{article.body}</p>
+  </div>
+</section>
+
     )
 }
 
-export default singleArticlePage;
+export default singleArticlePage;   
